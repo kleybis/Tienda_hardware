a8f2257fd701c60d14aab841fc1108729e783b8d
cley <cleybismatos@gmail.com>
2023-10-18 14:25:25 +0400

    Ejercicio6

--- EjemploCSV/src/ejercicio6Ficheros/modelo/datos/Cancion.java
+++ EjemploCSV/src/ejercicio6Ficheros/modelo/datos/Cancion.java
@@ -2,4 +2,8 @@ package ejercicio6Ficheros.modelo.datos;
 
 public class Cancion {
 
+	private int idCancion;
+	private String nombre;
+	private int duracion;
+	private String pais;
 }
--- EjemploCSV/src/ejercicio6Ficheros/modelo/datos/Musico.java
+++ EjemploCSV/src/ejercicio6Ficheros/modelo/datos/Musico.java
@@ -1,5 +1,14 @@
 package ejercicio6Ficheros.modelo.datos;
 
+import java.util.ArrayList;
+
 public class Musico {
 
+	private int idMusico;
+	private String nombre_comercial;
+	private String nombre_real;
+	private String nacionalidad;
+	
+	ArrayList<Cancion>listaCanciones=new ArrayList<Cancion>();
+	
 }
